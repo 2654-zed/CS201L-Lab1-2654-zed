6c8ba898cc44b0532fbc89abe0e9dbe9e9d825c5
2654-zed <jason2001trinh@gmail.com>
2026-09-01 09:06:15 -0500

    Finished the lab. I added code to get the other 2 values from the user, check if they are valid, generate the other 2 values, print the other point, calculate dist2, and determine which distance is closer to the origin and print it.

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-//NAME:  add your name here
-//DATE:  add the current date of your lab class
-//LAB TIME: add the time of your lab class
-//PROGRAM:
+//NAME:  Jason Trinh
+//DATE:  9/1/2026
+//LAB TIME: 8:00-10:30am
+//PROGRAM: Lab 1
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
@@ -26,11 +26,20 @@ public class Main {
             System.out.println("Enter the 4 values separated by spaces: ");
             x1 = input.nextInt();
             y1 = input.nextInt();
+    // add code to get the other 2 values from the user
+            x2 = input.nextInt();
+            y2 = input.nextInt();
 
             if (!((x1 >= -10 && x1 <= 10) &&
                 (y1 >= -10 && y1 <= 10))){
                 System.out.println("A value entered was not valid"); 
-                return;                       
+                return;
+            }
+    // add code to check if the other 2 values are valid
+            if (!((x2 >= -10 && x2 <= 10) &&
+                (y2 >= -10 && y2 <= 10))){
+                System.out.println("A value entered was not valid"); 
+                return;                      
             }
 
         }
@@ -38,6 +47,9 @@ public class Main {
         else if (choice == 'g') {
             x1 = (int)(Math.random() * 21) - 10;
             y1 = (int)(Math.random() * 21) - 10;
+        // add code to generate the other 2 values
+            x2 = (int)(Math.random() * 21) - 10;
+            y2 = (int)(Math.random() * 21) - 10;
          }
         else {
             System.out.println("The value " + choice + " is not a valid option!");
@@ -45,12 +57,24 @@ public class Main {
         }
         //print both points
         System.out.println("Point 1: (" + x1 + ", " + y1 + ")");
+        // print the other point
+        System.out.println("Point 2: (" + x2 + ", " + y2 + ")");
 
         //calculate dist1 & dist2 (the distance from each point to the origin)
         dist1 = Math.pow(Math.pow((x1-0),2) + Math.pow((y1-0),2),.5);
+        // add code to calculate dist2
+        dist2 = Math.pow(Math.pow((x2-0),2) + Math.pow((y2-0),2),.5);
 
         //determine which is distance closer to the origin and print
-
+        if (dist1 < dist2) {
+            System.out.println("Point 1 is closer to the origin than Point 2");
+        }
+        else if (dist1 > dist2) {
+            System.out.println("Point 2 is closer to the origin than Point 1");
+        }
+        else {
+            System.out.println("Both points are the same distance from the origin");
+        }
 
         //close scanner
         input.close();
